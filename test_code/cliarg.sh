cf2193dbf9742f19a66fcd2eb9bfc9b9cee0ece5
Sander Demeester <demeester.sander@gmail.com>
2011-08-09 13:17:34 +0200

    making basic chart based on example parsed dataset

--- test_code/cliarg.sh
+++ test_code/cliarg.sh
@@ -2,15 +2,17 @@
 
 divid=0
 contentheader=yes
-tmp=`getopt -o nd: --long no-content-header,div-id: -- $@`
+file=NULL
+tmp=`getopt -o nd:f: --long no-content-header,div-id:,file: -- $@`
 eval set -- $tmp
 while true ; do
     case $1 in
 	-n|--no-content-header) contentheader=no; shift;;
 	-d|--div-id) divid="$2"; shift;;
 	--) shift; break;;
-	*) break;;
+	*) exit 1;;
     esac
 done
 echo $contentheader
 echo $divid
+echo $file
