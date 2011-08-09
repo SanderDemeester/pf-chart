40d6701e742966474c926846dc58214154d52cee
Sander Demeester <demeester.sander@gmail.com>
2011-08-09 21:17:22 +0200

    adding DIV_IDNUMBER to the options struct

--- script2.js
+++ script2.js
@@ -1,9 +1,9 @@
     ]);
       // Set chart options
-    var options = {'title':'title_replace',
+    var optionsID_NUMBER = {'title':'title_replace',
                    'width':breedte_replace,
                    'height':hoogte_replace};
 
       // Instantiate and draw our chart, passing in some options.
     var chartID_NUMBER = new google.visualization.PieChart(document.getElementById('chart_div_ID_NUMBER'));
-    chartID_NUMBER.draw(data, options);
+    chartID_NUMBER.draw(data, optionsID_NUMBER);
